dd69a6c4546842183231d55b935ce6c4c14dc517
Jay <inforaamitsolutions@gmail.com>
2018-12-21 11:29:19 +0530

    test new changes

--- app/src/main/java/com/yakrm/codeclinic/yakrm/Activities/GiftDetailsActivity.java
+++ app/src/main/java/com/yakrm/codeclinic/yakrm/Activities/GiftDetailsActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.yakrm.codeclinic.yakrm.Adapter.GiftDetailListAdapter;
 import com.yakrm.codeclinic.yakrm.Models.GiftDetailListItemModel;
@@ -21,12 +23,22 @@ public class GiftDetailsActivity extends AppCompatActivity {
     ArrayList<String> ar_discount = new ArrayList<>();
     ArrayList<String> ar_pay = new ArrayList<>();
 
+    ImageView img_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gift_details);
 
         recyclerView = findViewById(R.id.recyclerView);
+        img_back = findViewById(R.id.img_back);
+
+        img_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
