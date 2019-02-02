80af8645192d4c0765f024ea082f44857ee59c53
Jay <inforaamitsolutions@gmail.com>
2019-02-02 14:50:13 +0530

    Few details Changes (Jay)

--- app/src/main/java/com/yakrm/codeclinic/Activities/CompletingPurchasingActivity.java
+++ app/src/main/java/com/yakrm/codeclinic/Activities/CompletingPurchasingActivity.java
@@ -109,7 +109,7 @@ public class CompletingPurchasingActivity extends AppCompatActivity {
                     @SuppressLint("StaticFieldLeak")
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-              /*          progressDialog.setMessage("Please Wait");
+                        progressDialog.setMessage("Please Wait");
                         progressDialog.setIndeterminate(true);
                         progressDialog.setCancelable(false);
                         progressDialog.show();
@@ -126,9 +126,39 @@ public class CompletingPurchasingActivity extends AppCompatActivity {
                                 progressDialog.dismiss();
                                 String status = response.body().getStatus();
                                 if (status.equals("1")) {
-                                    succesful_cardview.setVisibility(View.VISIBLE);
+                           /*         succesful_cardview.setVisibility(View.VISIBLE);
                                     scrollview_pay.setVisibility(View.GONE);
-                                    Toast.makeText(CompletingPurchasingActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(CompletingPurchasingActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();*/
+                                    Intent in = new Intent(getApplicationContext(), PayTabActivity.class);
+                                    in.putExtra(PaymentParams.MERCHANT_EMAIL, "ahmed@yakrm.com"); //this a demo account for testing the sdk
+                                    in.putExtra(PaymentParams.SECRET_KEY, "ex2SHCqdgtJlrF2gp5fGCis3tUGh5EkjcmcTZD7g6RCxwEOWJ3Cml4qOY664KroXOBQNeY3lPFTlkHh4KUq6YQVXW22HtrFh2w4g");//Add your Secret Key Here
+                                    in.putExtra(PaymentParams.LANGUAGE, PaymentParams.ENGLISH);
+                                    in.putExtra(PaymentParams.TRANSACTION_TITLE, "Paytabs");
+                                    in.putExtra(PaymentParams.AMOUNT, 5.0);
+
+                                    in.putExtra(PaymentParams.CURRENCY_CODE, "SAR");
+                                    in.putExtra(PaymentParams.CUSTOMER_PHONE_NUMBER, "009733");
+                                    in.putExtra(PaymentParams.CUSTOMER_EMAIL, "jaypokarjdp@gmail.com");
+                                    in.putExtra(PaymentParams.ORDER_ID, "123456");
+                                    in.putExtra(PaymentParams.PRODUCT_NAME, "Product 1, Product 2");
+//Billing Address
+                                    in.putExtra(PaymentParams.ADDRESS_BILLING, "Flat 1,Building 123, Road 2345");
+                                    in.putExtra(PaymentParams.CITY_BILLING, "Manama");
+                                    in.putExtra(PaymentParams.STATE_BILLING, "Manama");
+                                    in.putExtra(PaymentParams.COUNTRY_BILLING, "BHR");
+                                    in.putExtra(PaymentParams.POSTAL_CODE_BILLING, "00973"); //Put Country Phone code if Postal code not available '00973'
+//Shipping Address
+                                    in.putExtra(PaymentParams.ADDRESS_SHIPPING, "Flat 1,Building 123, Road 2345");
+                                    in.putExtra(PaymentParams.CITY_SHIPPING, "Manama");
+                                    in.putExtra(PaymentParams.STATE_SHIPPING, "Manama");
+                                    in.putExtra(PaymentParams.COUNTRY_SHIPPING, "BHR");
+                                    in.putExtra(PaymentParams.POSTAL_CODE_SHIPPING, "00973"); //Put Country Phone code if Postal code not available '00973'
+//Payment Page Style
+                                    in.putExtra(PaymentParams.PAY_BUTTON_COLOR, "#2474bc");
+                                    in.putExtra(PaymentParams.THEME, PaymentParams.THEME_LIGHT);
+
+                                    in.putExtra(PaymentParams.IS_TOKENIZATION, true);
+                                    startActivityForResult(in, PaymentParams.PAYMENT_REQUEST_CODE);
                                 } else {
                                     scrollview_pay.setVisibility(View.GONE);
                                     error_cardview.setVisibility(View.VISIBLE);
@@ -143,38 +173,7 @@ public class CompletingPurchasingActivity extends AppCompatActivity {
                                 error_cardview.setVisibility(View.VISIBLE);
                                 Toast.makeText(CompletingPurchasingActivity.this, "Server Error", Toast.LENGTH_SHORT).show();
                             }
-                        });*/
-
-                        Intent in = new Intent(getApplicationContext(), PayTabActivity.class);
-                        in.putExtra(PaymentParams.MERCHANT_EMAIL, "ahmed@yakrm.com"); //this a demo account for testing the sdk
-                        in.putExtra(PaymentParams.SECRET_KEY, "ex2SHCqdgtJlrF2gp5fGCis3tUGh5EkjcmcTZD7g6RCxwEOWJ3Cml4qOY664KroXOBQNeY3lPFTlkHh4KUq6YQVXW22HtrFh2w4g");//Add your Secret Key Here
-                        in.putExtra(PaymentParams.LANGUAGE, PaymentParams.ENGLISH);
-                        in.putExtra(PaymentParams.TRANSACTION_TITLE, "Paytabs");
-                        in.putExtra(PaymentParams.AMOUNT, 5.0);
-
-                        in.putExtra(PaymentParams.CURRENCY_CODE, "SAR");
-                        in.putExtra(PaymentParams.CUSTOMER_PHONE_NUMBER, "009733");
-                        in.putExtra(PaymentParams.CUSTOMER_EMAIL, "jaypokarjdp@gmail.com");
-                        in.putExtra(PaymentParams.ORDER_ID, "123456");
-                        in.putExtra(PaymentParams.PRODUCT_NAME, "Product 1, Product 2");
-//Billing Address
-                        in.putExtra(PaymentParams.ADDRESS_BILLING, "Flat 1,Building 123, Road 2345");
-                        in.putExtra(PaymentParams.CITY_BILLING, "Manama");
-                        in.putExtra(PaymentParams.STATE_BILLING, "Manama");
-                        in.putExtra(PaymentParams.COUNTRY_BILLING, "BHR");
-                        in.putExtra(PaymentParams.POSTAL_CODE_BILLING, "00973"); //Put Country Phone code if Postal code not available '00973'
-//Shipping Address
-                        in.putExtra(PaymentParams.ADDRESS_SHIPPING, "Flat 1,Building 123, Road 2345");
-                        in.putExtra(PaymentParams.CITY_SHIPPING, "Manama");
-                        in.putExtra(PaymentParams.STATE_SHIPPING, "Manama");
-                        in.putExtra(PaymentParams.COUNTRY_SHIPPING, "BHR");
-                        in.putExtra(PaymentParams.POSTAL_CODE_SHIPPING, "00973"); //Put Country Phone code if Postal code not available '00973'
-//Payment Page Style
-                        in.putExtra(PaymentParams.PAY_BUTTON_COLOR, "#2474bc");
-                        in.putExtra(PaymentParams.THEME, PaymentParams.THEME_LIGHT);
-
-                        in.putExtra(PaymentParams.IS_TOKENIZATION, true);
-                        startActivityForResult(in, PaymentParams.PAYMENT_REQUEST_CODE);
+                        });
                     }
                 }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
                     @Override
