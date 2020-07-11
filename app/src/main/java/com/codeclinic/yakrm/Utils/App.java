d89919b686753fd8984b280be02b5ff7d0493df2
krutik <krutik.poojara@gmail.com>
2020-07-11 10:58:07 +0530

    Messaege added in both language

--- app/src/main/java/com/codeclinic/yakrm/Utils/App.java
+++ app/src/main/java/com/codeclinic/yakrm/Utils/App.java
@@ -1,8 +1,12 @@
 package com.codeclinic.yakrm.Utils;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.res.Configuration;
+import android.os.Bundle;
 
+import com.adjust.sdk.Adjust;
+import com.adjust.sdk.AdjustConfig;
 import com.franmontiel.localechanger.LocaleChanger;
 
 import static com.codeclinic.yakrm.Utils.CommonMethods.SUPPORTED_LOCALES;
@@ -12,12 +16,58 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         LocaleChanger.initialize(getApplicationContext(), SUPPORTED_LOCALES);
+
+        String appToken = "dczclrbzx4w0";
+        String environment = AdjustConfig.ENVIRONMENT_SANDBOX;
+        //String environment = AdjustConfig.ENVIRONMENT_PRODUCTION;
+        AdjustConfig config = new AdjustConfig(this, appToken, environment);
+        Adjust.onCreate(config);
+
+        registerActivityLifecycleCallbacks(new AdjustLifecycleCallbacks());
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         LocaleChanger.onConfigurationChanged();
+
+    }
+
+    private static final class AdjustLifecycleCallbacks implements ActivityLifecycleCallbacks {
+        @Override
+        public void onActivityCreated(Activity activity, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onActivityStarted(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {
+            Adjust.onResume();
+        }
+
+        @Override
+        public void onActivityPaused(Activity activity) {
+            Adjust.onPause();
+        }
+
+        @Override
+        public void onActivityStopped(Activity activity) {
+
+        }
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+
+        }
     }
 
 }
