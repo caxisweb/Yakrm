44a6c34732856ac70c7a8ccd7f068955d4f1d50f
krutik <krutik.poojara@gmail.com>
2020-08-13 14:33:33 +0530

    Messaege added in both language

--- app/src/main/java/com/codeclinic/yakrm/Utils/ImageURL.java
+++ app/src/main/java/com/codeclinic/yakrm/Utils/ImageURL.java
@@ -1,8 +1,8 @@
 package com.codeclinic.yakrm.Utils;
 
 public class ImageURL {
-    //static String ImageBaseURL = "http://test.yakrm.com/assets/uploads/";
-    static String ImageBaseURL = "https://www.yakrm.com/assets/uploads/";
+    static String ImageBaseURL = "http://test.yakrm.com/assets/uploads/";
+    //static String ImageBaseURL = "https://www.yakrm.com/assets/uploads/";
     public static String profile_img_url = ImageBaseURL + "user_profile/";
     public static String Vendor_brand_image = ImageBaseURL + "brand_images/";
     public static String Vendor_voucher_image = ImageBaseURL + "voucher_images/";
